de2ae81140f750ec02b1b725eb968d7cc9353ce9
fozo.david@gmail.com <fozo.david@gmail.com>
2016-02-07 11:00:59 +0100

    Binary Search Tree implementation

---
+++ crackingcode/Trees/bsttree.cpp
@@ -0,0 +1,18 @@
+//bsttree.cpp
+
+struct Node{
+	int data;
+	Node* left;
+	Node* right;
+};
+void insert(Node* root, int data){
+
+}
+
+int main(int argc, char const *argv[]){
+	Node* rootPtr = NULL;
+
+
+
+	return 0;
+}
